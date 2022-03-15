1499419fc55a0985c5956492d86c6f4239ed29b5
Elcaam23 <101618460+Elcaam23@users.noreply.github.com>
2022-03-14 23:56:48 -0500

    Add files via upload

---
+++ ATM-codes.cpp
@@ -0,0 +1,49 @@
+
+ 
+#include <iostream>
+using namespace std;
+ 
+void showMenu (){
+    cout <<"**********MENU**********"<<endl;
+    cout<<"1. Check Amount"<<endl;
+    cout<<"2. Deposit"<<endl;
+    cout<<"3. Withdraw"<<endl;
+    cout<<"4. Exit"<<endl;
+    cout<<"********************"<<endl;
+}
+int main ()
+{
+    //check balance, deposit, withdraw, show menu
+    int option;
+    double Amount = 50000;
+    
+    do {
+    showMenu();
+    cout<<"option";
+    cin>>option;
+   
+    switch (option){
+        case 1:cout<<"Amount is:"<<Amount<<"$"<<endl;break;
+        case 2:cout<<"deposit amount:";
+            double depositAmount;
+            cin>>depositAmount;
+            Amount += depositAmount;
+            break;
+        case 3: cout<<"withdraw amount:";
+            double withdrawAmount;
+            cin>>withdrawAmount;
+            if (withdrawAmount <= Amount)
+                Amount -= withdrawAmount;
+            else
+                cout<<"You don't have enough money on your account"<<endl;
+            break;
+    }
+    } while (option!=4);
+    
+        system("pause>0");
+    }
+            
+            
+            
+ 
+
